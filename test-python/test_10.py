c3543576abe61cb8cb843d14b30f80fa46248326
Manuel Marin <manuel.duque5.2@gmail.com>
2022-09-02 13:15:52 -0500

    feature: Test 10.

--- test-python/test_10.py
+++ test-python/test_10.py
@@ -3,3 +3,24 @@
   información y solo muestre el nombre y la identificación, 
   ordenelos por identificación
 """
+
+from copy import deepcopy
+from data import get_thirds
+from test_3 import get_third_name
+
+
+def show_min_third_info(thirds: list[dict]):
+  new_thirds = []
+  for third in thirds:
+    if tradename:= third.get('tradename', None):
+      name = tradename
+    else:
+      name = get_third_name(third)
+    new_third = {'name': name, 'identification': third.get('identificationNumber', None)}
+    new_thirds.append(new_third)
+  return sorted(new_thirds, key=lambda x: x['identification'])
+
+
+if __name__ == '__main__':
+  thirds: list[dict] = get_thirds()
+  print(show_min_third_info(deepcopy(thirds)))
